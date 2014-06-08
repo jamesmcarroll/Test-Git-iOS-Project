6c34eade0e2b94182d5ec05f42ff91735a95eeb6
James Carroll <jamescarroll@Jamess-MacBook-Pro.local>
2014-06-08 12:02:10 -0500

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by James Carroll on 6/8/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by James Carroll on 6/8/14.
+//
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
